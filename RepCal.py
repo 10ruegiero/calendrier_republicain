83ddc52a8913da53cfe443d81c69c8c542e4b427
Victor di Ruggiero <victor@diruggiero.fr>
2018-07-28 17:16:51 +0200

    Modification de la fonction date_republicaine_depuis_lan_I

--- RepCal.py
+++ RepCal.py
@@ -18,32 +18,34 @@ def nbr_jours_depuis_le_22_septembre_1792(date_greg):
 # Fonction déroulant le calendrier républicain depuis le 1er Vendémiaire de l'an I, en fonction du nombre de jours demandés
 def date_republicaine_depuis_lan_I(jours):
 
-
-
-    # Création d'un objet date républicaine
-    date_rep = daterepublicaine(1,1,1)
-
     jours_ecoulees = 0
+    r_year = 1
+    r_month = 1
+    r_day = 1
+
+    # Parcours du calendrier républicain
     while jours_ecoulees < jours:
         jours_ecoulees += 1
 
-        if date_rep.month < 13:
-            if date_rep.day == 30:
-                date_rep.day = 0
-                date_rep.month +=1
-        if date_rep.month == 13:
-            if date_rep.est_bissextile():
-                if date_rep.day == 6 :
-                    date_rep.day = 0
-                    date_rep.month = 1
-                    date_rep.year +=1
+        if r_month < 13:
+            if r_day == 30:
+                r_day = 0
+                r_month +=1
+        if r_month == 13:
+            if daterepublicaine.est_bissextile(r_year):
+                if r_day == 6 :
+                    r_day = 0
+                    r_month = 1
+                    r_year +=1
             else:
-                if date_rep.day == 5 :
-                    date_rep.day = 0
-                    date_rep.month = 1
-                    date_rep.year += 1
-        date_rep.day += 1
+                if r_day == 5 :
+                    r_day = 0
+                    r_month = 1
+                    r_year += 1
+        r_day += 1
 
+    # Création d'un objet date républicaine
+    date_rep = daterepublicaine(r_year,r_month,r_day)
     return date_rep
 
 # Fonction Main
