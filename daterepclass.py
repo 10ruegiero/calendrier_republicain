640230dee646a6a7359ce18bf9f68c133b1836d0
Victor di Ruggiero <victor@diruggiero.fr>
2018-07-28 17:16:29 +0200

    Modification de la fonction __str__

--- daterepclass.py
+++ daterepclass.py
@@ -4,16 +4,17 @@ import datetime
 
 # Définition d'un objet date républicaine
 class daterepublicaine(datetime.date):
-    # Liste des 12 mois républicains
-    # (13 mois avec les Sansculottides, 5 jours additionnels en fin d'année, 6 les années bisexxtiles)
-    mois_rep = ["Vendémiaire", "Brumaire", "Frimaire", "Nivôse", "Pluviôse", "Ventôse", "Germinal", "Floréal",
-                "Prairial", "Messidor", "Thermidor", "Fructidor", "Sanculottides"]
-
     def __init__(self,year,month,day):
         self.replace(year,month,day)
 
     def __str__(self):
-        date_text = "%i %s de l'an %i" % (self.day, mois_rep[self.month - 1], date_republicaine[2])
+        # Liste des 12 mois républicains
+        # (13 mois avec les Sansculottides, 5 jours additionnels en fin d'année, 6 les années bisexxtiles)
+        mois_rep = ["Vendémiaire", "Brumaire", "Frimaire", "Nivôse", "Pluviôse", "Ventôse", "Germinal", "Floréal",
+                    "Prairial", "Messidor", "Thermidor", "Fructidor", "Sanculottides"]
+
+        #Formatage de la date républicaine
+        date_text = "%i %s de l'an %i" % (self.day, mois_rep[self.month - 1], self.year)
         return date_text
 
     def suis_bissextile(self):
