ee6b610565e09a2e1830d2d18e525da0d84b63e6
Victor di Ruggiero <victor@diruggiero.fr>
2018-07-28 16:25:29 +0200

    Branche orientée objet : création d'un objet daterepublicaine, héritant de datetime

---
+++ daterepublicaine.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# Import des modules time et datetime
+import datetime
+
+# Définition d'un objet date républicaine
+class daterepublicaine(datetime.datetime):
+    # Liste des 12 mois républicains
+    # (13 mois avec les Sansculottides, 5 jours additionnels en fin d'année, 6 les années bisexxtiles)
+    mois_rep = ["Vendémiaire", "Brumaire", "Frimaire", "Nivôse", "Pluviôse", "Ventôse", "Germinal", "Floréal",
+                "Prairial", "Messidor", "Thermidor", "Fructidor", "Sanculottides"]
+
+    def __init__(self,year,month,day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def __str__(self):
+        date_text = "%i %s de l'an %i" % (self.day, mois_rep[self.month - 1], date_republicaine[2])
+        return date_text
