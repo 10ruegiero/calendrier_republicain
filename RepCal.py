44bfadbd0131cf417eb202d8b7eed26f0f751034
Victor di Ruggiero <victor@diruggiero.fr>
2018-07-28 16:40:34 +0200

    Modification de la structure de la fonction centrale de RepCal

--- RepCal.py
+++ RepCal.py
@@ -17,41 +17,34 @@ def nbr_jours_depuis_le_22_septembre_1792(date_greg):
 
 # Fonction déroulant le calendrier républicain depuis le 1er Vendémiaire de l'an I, en fonction du nombre de jours demandés
 def date_republicaine_depuis_lan_I(jours):
-    # Le calendrier commence le 1er Vendémiaire de l'an I
-    decade = 0
-    r_jour = 1
-    r_mois = 1
-    r_annee = 1
+
+
+
+    # Création d'un objet date républicaine
+    date_rep = daterepublicaine.date(1,1,1)
 
     jours_ecoulees = 0
-
     while jours_ecoulees < jours:
         jours_ecoulees += 1
 
-        if r_mois < 13:
-            if r_jour == 30:
-                r_jour = 0
-                r_mois +=1
-        if r_mois == 13:
-            if rep_est_bissextile(r_annee):
-                if r_jour == 6 :
-                    r_jour = 0
-                    r_mois = 1
-                    r_annee +=1
+        if date_rep.month < 13:
+            if date_rep.day == 30:
+                date_rep.day = 0
+                date_rep.month +=1
+        if date_rep.month == 13:
+            if date_rep.est_bissextile():
+                if date_rep.day == 6 :
+                    date_rep.day = 0
+                    date_rep.month = 1
+                    date_rep.year +=1
             else:
-                if r_jour == 5 :
-                    r_jour = 0
-                    r_mois = 1
-                    r_annee += 1
+                if date_rep.day == 5 :
+                    date_rep.day = 0
+                    date_rep.month = 1
+                    date_rep.year += 1
+        date_rep.day += 1
 
-        r_jour += 1
-
-    date_republicaine = [r_jour,r_mois,r_annee]
-    return date_republicaine
-
-# Fonction transformation un date_republicaine sous format string
-def format_date_republicaine(date_republicaine):
-
+    return date_rep
 
 # Fonction Main
 def main():
@@ -70,9 +63,6 @@ def main():
 
     # Parcours fictif du calendrier républicain
     date_rep = date_republicaine_depuis_lan_I(jours_depuis_le_1er_vendemiaire.days)
-
-    # Formatage de la date en texte
-    date_rep = format_date_republicaine(date_rep)
 
     #Sortie console
     print(date_rep)
